049b72cfbadc6137d09521bf2d3334a62610eee6
Your Name <you@example.com>
2024-11-12 23:11:25 +0800

    11:11

--- php/login.php
+++ php/login.php
@@ -1,7 +1,20 @@
 <?php
 session_start();
 header('Content-Type: application/json');
-include_once "config.php";
+
+  $hostname = "mysql.hostinger.com";
+  $username = "new";
+  $password = "Root_root01";
+  $dbname = "u412427249_capstone";
+
+  $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  if(!$conn){
+    echo "Database connection error: ".mysqli_connect_error();
+  }
+
+
+
+
 
 // Enable error reporting for debugging (remove in production)
 ini_set('display_errors', 1);
