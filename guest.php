147fda280d06426fa064e8dc603aa229c79a39b4
Your Name <you@example.com>
2024-11-19 06:59:53 +0800

    .

--- guest.php
+++ guest.php
@@ -601,7 +601,14 @@
 					<ul class="splide__list">
 
 						<?php
-							include_once "php/config.php";
+							$hostname = "snow-bear-572093.hostingersite.com";
+							$username = "new";
+							$password = "Root_root01";
+							$dbname = "u412427249_capstone";
+
+							$conn = mysqli_connect($hostname, $username, $password, $dbname);
+							if(!$conn){
+								echo "Database connection error: ".mysqli_connect_error();
 
 							
 							$date = isset($_GET['date']) ? $_GET['date'] : '';
