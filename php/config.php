2883f908b05a939c4b54139392e34dcfaadfdd51
Your Name <you@example.com>
2024-11-21 04:23:01 +0800

    Nov. 21_2024

--- php/config.php
+++ php/config.php
@@ -1,11 +1,14 @@
 <?php
-  $hostname = "127.0.0.1";
-  $username = "new";
-  $password = "Root_root01";
-  $dbname = "u412427249_capstone";
+	$hostname = "localhost";
+	$username = "new";
+	$password = "Root_root01";
+	$dbname = "u412427249_capstone";
 
-  $conn = mysqli_connect($hostname, $username, $password, $dbname);
-  if(!$conn){
-    echo "Database connection error: ".mysqli_connect_error();
-  }
+	$conn = mysqli_connect($hostname, $username, $password, $dbname);
+	if(!$conn){
+	echo "Database connection error: ".mysqli_connect_error();
+	}
+
+	echo "Connected successfully";
+	mysqli_close($conn);
 ?>
