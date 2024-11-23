b6691827461f64861e2a5cb8b0f4f36d82493748
Your Name <you@example.com>
2024-11-23 17:51:20 +0800

    Nov. 23_2024

--- report.php
+++ report.php
@@ -166,11 +166,6 @@
 						$sql .= " ORDER BY CreatedTime DESC;";
 						$reportResult = $conn->query($sql);
 
-						if ($reportResult->num_rows == 0) {
-							echo "<p>No incidents found for " . $eventDate . ".</p>";
-							continue;
-						}
-
 						echo "<div class='card-container'>";
 						echo "<span class='date'>" . $eventDate . "</span>";
 						echo "<div class='card-grid'>";
