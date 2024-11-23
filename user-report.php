a0f8cba33cbbe31b5ba9be8607a34dc89cab3fa1
Your Name <you@example.com>
2024-11-23 22:29:45 +0800

    Nov. 23_2024

--- user-report.php
+++ user-report.php
@@ -133,14 +133,14 @@
 				}
 
 				foreach ($eventDates as $eventDate) {
-					$formattedDate = $conn->real_escape_string(date("Y-m-d", strtotime($eventDate)));
-					$sql = "SELECT ir.IncidentReportID, ir.Zone, ir.Street, ir.CreatedAt, it.IncidentTypeName, b.BarangayName
-							FROM incident_report AS ir
-							LEFT JOIN incident_type AS it ON ir.IncidentTypeID = it.IncidentTypeID
-							LEFT JOIN barangay AS b ON ir.BarangayID = b.BarangayID
-							WHERE ir.CreatedAt = '$formattedDate' AND ir.OfficialsID = $user_id
-							ORDER BY ir.CreatedTime DESC
-						";
+$formattedDate = date("Y-m-d", strtotime($eventDate));
+$sql = "SELECT ir.IncidentReportID, ir.Zone, ir.Street, ir.CreatedAt, it.IncidentTypeName, b.BarangayName
+        FROM incident_report AS ir
+        LEFT JOIN incident_type AS it ON ir.IncidentTypeID = it.IncidentTypeID
+        LEFT JOIN barangay AS b ON ir.BarangayID = b.BarangayID
+        WHERE DATE(ir.CreatedAt) = '$formattedDate' AND ir.OfficialsID = $user_id
+        ORDER BY ir.CreatedTime DESC";
+
 					$reportResult = $conn->query($sql);
 
 					if (!$reportResult) {
