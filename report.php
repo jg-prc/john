03fa0ce72bca4d71915f67fea8a1e5695083d243
Your Name <you@example.com>
2024-11-25 00:01:10 +0800

    Nov. 24_2024

--- report.php
+++ report.php
@@ -143,6 +143,34 @@
 
 						if ($reportResult->num_rows > 0) {
 
+						while ($row = $reportResult->fetch_assoc()) {
+							$icon = '';
+							switch ($row['IncidentTypeName']) {
+								case 'Vehicular Accident':
+									$icon = '<i class="fas fa-car-crash"></i>';
+									break;
+								case 'Fire Incident':
+									$icon = '<i class="fas fa-fire"></i>';
+									break;
+								case 'Flood Incident':
+									$icon = '<i class="fas fa-house-flood-water"></i>';
+									break;
+								case 'Landslide Incident':
+									$icon = '<i class="fas fa-hill-rockslide"></i>';
+									break;
+							}
+							echo "
+								<a class='card' href='report_file.php?report_id=" . $row['IncidentReportID'] . "'>
+									<div class='image'>
+										" . $icon . "
+									</div>
+									<div class='details'>
+										<span class='type'>" . $row['IncidentTypeName'] . "</span>
+										<span>Zone " . $row['Zone'] . " , " . $row['BarangayName'] . "</span>
+									</div>
+								</a>
+							";
+						}
 						} else {
 							echo "<div class='no-data'>No reports available for " . htmlspecialchars($eventDate) . ".</div>";
 						}
