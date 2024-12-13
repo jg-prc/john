c89a945ab838a2389b820217855a6ae8cbdb5ad2
Your Name <you@example.com>
2024-12-13 13:46:58 +0800

    Dec. 13_2024

--- map.php
+++ map.php
@@ -30,20 +30,6 @@
 
 
 				<path id="adiangao" class="map" transform="translate(1681,1083)" d="m0 0 7 6 7 7 8 7 8 8 2 1 3-9 2-3h10l14 1 3 8 5 18v4l-9 3 2 24 4 9-1 2 1 10 1 9-8 19-4 9v6l3 5v10l-2 11-2 6-1 17-10 17-17 10h-2l2 14v9l-3 2-1 9-3 5-2 1h-8l-2 4-7 10-1 3v12l3 7-1 5-13 25-7 11-3 3-3 6-3 3-7 1-3 4-1 7v10l-5 5-4 1-8-5-3-1h-7l-5 3-7 8-11 17-11 7-6 7-5 11-6 12-4 4h-2l-4-6-8-4-16-3h-28l-16 1-4-5-3-1h-18l-5-8-4-2-7-1-18-1-3-2h-8l-7 3-7 8-11 8-9 5-4-2-3-2-10 1h-14l-10-5-3-3 1-11-1-3-9-2-8-6-12-15-12-8-6-9-7-8-7-7-9-16-3-12-2-24-1-37 5 2 3 1 2-9 16-51 8-5 2-10 5-8 4-9 3-1 12-3 16-8 2-2-2-2 1-4 13-21 28-14 9-6 7-8 13-13 8-7 8-8h2l4 4 16 5 27 4h11l49-20 6-3 5-5 14 3 2-4 2 3 3 5 6 4 6-2 15-6 34-10 20-5 4 2 4 4 10-2 7-1 6-36 2-11z" fill="#00E272"/>
-				<path transform="translate(1522,1006)" d="m0 0h13l4 2 1 2v6l-2 3 3 1 1 5-3 5-3 1h-14zm4 3v8h8l3-2-1-5-1-1zm0 11v8h10l2-2-1-5-1-1z" fill="#020202"/>
-				<path transform="translate(1631,1013)" d="m0 0h6 3 3v18l-2 5-3 2h-7l-4-3v-3l4 2h7l1-4-7 1-5-4-1-8 3-5zm3 2-4 3-1 5 3 5h5l3-4-1-6z" fill="#020202"/>
-				<path transform="translate(1332,1006)" d="m0 0h8l5 3 1 4h-4l-2-3h-8l-1 4 12 4 3 3v6l-5 4h-9l-5-4-1-5h3l2 4 2 1h8l1-5-12-4-3-3v-5z" fill="#020202"/>
-				<path transform="translate(1375,1013)" d="m0 0 3 1 2-1h6l4 4 1 7-3 5-2 2h-6l-2-1v7h-4v-23zm7 2-5 5 1 6 2 2h5l2-2v-8z" fill="#020202"/>
-				<path transform="translate(1436,1013)" d="m0 0h9l3 4v14l-4-1-2 1h-8l-3-5 2-4 6-2h5v-4h-8v2l-4-1zm2 10-3 2 1 3h6l2-2v-3z" fill="#020202"/>
-				<path transform="translate(1500,1006)" d="m0 0h3v25l-4-1-1 1h-6l-4-4-1-2 1-9 3-3h7l1 1zm-5 9-4 2-1 7 3 4h5l2-2v-8z" fill="#020202"/>
-				<path transform="translate(1587,1013)" d="m0 0h9l3 3v15l-4-1-2 1h-7l-3-2v-6l3-2 9-2v-3l-7 1-5 1 2-4zm2 10-3 2 1 3h6l3-3v-2z" fill="#020202"/>
-				<path transform="translate(1470,1013)" d="m0 0h8l4 4v6h-13l2 4h6l4-2v4l-3 2h-8l-4-4-1-7 3-6zm3 2-4 3v2h10l-2-4z" fill="#020202"/>
-				<path transform="translate(1400,1013)" d="m0 0h9l3 3v15l-4-1-2 1h-8l-2-2v-6l3-2 9-2v-3h-8v2l-4-1zm2 10-3 2 1 3h6l3-3v-2z" fill="#020202"/>
-				<path transform="translate(1653,1013)" d="m0 0h9l2 2 1 16-5-1-2 1h-7l-3-3 1-5 2-2 10-1-1-3-4-1-4 2h-3l2-4zm2 10-3 1v4h7l2-2v-3z" fill="#020202"/>
-				<path transform="translate(1551,1013)" d="m0 0h9l3 3 1 15-5-1-2 1h-7l-3-2 1-7 7-2h5l-1-3-4-1-4 2h-3l1-4zm2 10-3 2 1 3h7l2-5z" fill="#020202"/>
-				<path transform="translate(1356,1013)" d="m0 0h8l4 4 1 5-1 1h-13l2 4h7l3-2v4l-3 2h-7l-5-4v-10zm4 2-5 3v2h10l-1-4z" fill="#020202"/>
-				<path transform="translate(1670,1013)" d="m0 0h3l4 10v3h2l1-6 3-7h3l-3 10-6 14-6 1v-3h4l-1-9-4-10z" fill="#020202"/>
-				<path transform="translate(1606,1013)" d="m0 0 3 1 2-1h7l3 4v14h-3l-1-11-1-3-3-1-3 2-1 13h-3z" fill="#020202"/>
 				<path transform="translate(1641,1580)" d="m0 0h20v5l-2 1h-18l-1-5z" fill="#020202"/>
 				<path transform="translate(1456,1580)" d="m0 0h20l1 5-2 1h-18l-1-1z" fill="#020202"/>
 				<path transform="translate(1426,1580)" d="m0 0h20v5l-2 1h-18l-1-5z" fill="#020202"/>
@@ -118,12 +104,7 @@
 				<path transform="translate(1292,961)" d="m0 0h20v5h-20z"/>
 				<path transform="translate(1261,961)" d="m0 0h20v5h-20z"/>
 				<path transform="translate(1231,1560)" d="m0 0h5l1 18-5 2-1-2z"/>
-				<path transform="translate(1457,1007)" d="m0 0h1v6h3v3h-3v12l4 1v2h-6l-1-1v-14h-2v-3h2v-5z" fill="#020202"/>
 				<path transform="translate(1238,961)" d="m0 0h13v5l-12 1-3 2-3-2 2-4z" fill="#020202"/>
-				<path transform="translate(1419,1013)" d="m0 0h9v3l-5 1-1 14-4-1v-16z" fill="#020202"/>
 				<path transform="translate(1789,1569)" d="m0 0h5v10l-4 5-4 1-1-5 3-3z" fill="#020202"/>
 				<path transform="translate(1783,961)" d="m0 0 6 1 4 4 1 2v7h-5l-2-7-4-2z"/>
-				<path transform="translate(1570,1013)" d="m0 0h9v3h-5l-1 15h-3z" fill="#020202"/>
-				<path transform="translate(1606,1013)" d="m0 0 5 1-1 4-1 13h-3z" fill="#020202"/>
-
 			</svg>
